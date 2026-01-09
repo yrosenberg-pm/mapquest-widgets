c47809da48dffe9919941937db611e548d0a657d
Yosef Rosenberg <yosef.rosenberg@Mac.localdomain>
2026-01-09 10:41:42 -0800

    Further reduce pin shadow intensity (0.08 opacity)

--- components/widgets/MapQuestMap.tsx
+++ components/widgets/MapQuestMap.tsx
@@ -479,7 +479,7 @@ export default function MapQuestMap({
           <img src="${marker.iconUrl}" 
                width="${size[0]}" 
                height="${size[1]}" 
-               style="filter: drop-shadow(0 1px 2px rgba(0,0,0,0.15)); border-radius: 50%;"
+               style="filter: drop-shadow(0 1px 1px rgba(0,0,0,0.08)); border-radius: 50%;"
                alt=""
           />
         `;
